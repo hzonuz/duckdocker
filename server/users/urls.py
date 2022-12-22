afa5de0292a3195721e7fefa232367fe89180e93
Hasan Zonouzi <hasanzon@yahoo.com>
2022-12-22 23:29:23 +0330

    feat: user apis added

---
+++ server/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from users.views import SignUp, Login, SignOut, UserView
+
+app_name = 'users'
+urlpatterns = [
+    path('signup/v0/', SignUp.as_view(), name='signup-v0'),
+    path('login/v0/', Login.as_view(), name='login-v0'),
+    path('signout/v0/', SignOut.as_view(), name='signout-v0'),
+    path('me/v0/', UserView.as_view(), name='user-info-v0'),
+]
