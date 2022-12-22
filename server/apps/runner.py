f94b006598cc513ca8a794246e2a837fc5567b0d
Hasan Zonouzi <hasanzon@yahoo.com>
2022-12-22 23:55:26 +0330

    feat: docker runner added

---
+++ server/apps/runner.py
@@ -0,0 +1,18 @@
+import docker
+
+def run_container(image, command, envs):
+    client = docker.from_env()
+    container = client.containers.run(
+        image=image,
+        command=command,
+        environment=envs,
+        detach=True
+    )
+    return container
+
+def stop_container(container_id):
+    client = docker.from_env()
+    container = client.containers.get(container_id)
+    container.stop()
+    container.remove()
+
