5d5a5748fd2c4031a0d1222942f4a8a39662ea1b
Hasan Zonouzi <hasanzon@yahoo.com>
2022-12-22 23:46:37 +0330

    refactor: format code with black

--- server/users/urls.py
+++ server/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from users.views import SignUp, Login, SignOut, UserView
 
-app_name = 'users'
+app_name = "users"
 urlpatterns = [
-    path('signup/v0/', SignUp.as_view(), name='signup-v0'),
-    path('login/v0/', Login.as_view(), name='login-v0'),
-    path('signout/v0/', SignOut.as_view(), name='signout-v0'),
-    path('me/v0/', UserView.as_view(), name='user-info-v0'),
+    path("signup/v0/", SignUp.as_view(), name="signup-v0"),
+    path("login/v0/", Login.as_view(), name="login-v0"),
+    path("signout/v0/", SignOut.as_view(), name="signout-v0"),
+    path("me/v0/", UserView.as_view(), name="user-info-v0"),
 ]
