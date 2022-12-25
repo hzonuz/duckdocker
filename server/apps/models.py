785cd467747a5ea7a19b7033c79dcf88d6cc2097
Hasan Zonouzi <hasanzon@yahoo.com>
2022-12-25 15:01:02 +0330

    feat: change app and container model

--- server/apps/models.py
+++ server/apps/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class App(models.Model):
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="apps")
     name = models.CharField(max_length=128)
-    image = models.URLField()
+    image = models.CharField(max_length=128)
     command = models.CharField(max_length=128, blank=True, null=True)
     envs = models.JSONField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,7 +25,7 @@ class Container(models.Model):
     container_id = models.CharField(max_length=128)
     status = models.PositiveIntegerField(choices=Status.choices, default=Status.CREATED)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    stopped_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.container_id
