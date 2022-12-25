97aa3a8a936b18140b111eede907ce787a0cc332
Hasan Zonouzi <hasanzon@yahoo.com>
2022-12-25 15:01:41 +0330

    fix: run container parameters fixed

--- server/apps/runner.py
+++ server/apps/runner.py
@@ -1,12 +1,12 @@
 import docker
 
 
-def run_container(image, command, envs):
+def run_container(app):
     client = docker.from_env()
     container = client.containers.run(
-        image=image, command=command, environment=envs, detach=True
+        image=app.image, command=app.command, environment=app.envs, detach=True
     )
-    return container
+    return container.id
 
 
 def stop_container(container_id):
